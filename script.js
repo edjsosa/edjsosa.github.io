370bac9fa756eefbb10cfa0d5d4f807ffd29a05f
edjsosa <74710365+edjsosa@users.noreply.github.com>
2021-01-08 22:25:56 -0500

    2nd Draft

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 /* prints "hi" in the browser's dev tools console */
 console.log("hi");
+
+function display_update() {
+  let x = document.lastModified;
+  document.write("Last Updated: ", x);
+}
+
+display_update();
